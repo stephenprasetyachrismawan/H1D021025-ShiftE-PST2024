19db85cce29f149b5dc4b3420eb5ebbb47a72e04
stephenprasetyachrismawan <80116750+stephenprasetyachrismawan@users.noreply.github.com>
2024-04-29 14:10:17 +0700

    MotorServo

---
+++ Servo/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <Servo.h>
+// put function declarations here:
+Servo myservo;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(D4);
+}
+
+void loop() {
+  myservo.write(90);
+  for (int pos = 0; pos<=180; pos+=1){
+    myservo.write(pos);
+    delay(15);
+  }
+  for (int pos = 180; pos>=0; pos-=1){
+    myservo.write(pos);
+    delay(15);
+  }
+}
+
